31e5e4f5dca9ccb65dc7cd54644f9c12f4499560
ssn2013 <ssn2009@gmail.com>
2015-03-21 18:28:29 -0400

    Basic framework for the server

---
+++ HeatMapServer/deploy.sh
@@ -0,0 +1,4 @@
+rm -rf /opt/apache-tomcat-6.0.43/webapps/heatmapserver /opt/apache-tomcat-6.0.43/webapps/heatmapserver.war
+echo "Removed from tomcat webapps" 
+ant servlet-war
+mv heatmapserver.war /opt/apache-tomcat-6.0.43/webapps
