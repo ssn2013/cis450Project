23368e85cac8d2e4e1b1575f73a360cfde8057f0
ssn2013 <ssn2009@gmail.com>
2015-03-22 16:18:52 -0400

    latest changes

--- ImportDatabase/src/com/datformers/JSONParser.java
+++ ImportDatabase/src/com/datformers/JSONParser.java
@@ -1,6 +1,9 @@
-package com.datformers;
-
 import java.io.BufferedReader;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.io.File;
 import java.io.FileReader;
 import java.nio.charset.Charset;
@@ -11,13 +14,185 @@ import org.json.*;
 
 public class JSONParser {
 	public static void main(String[] args){
-		//business();
-		//users();
-		//checkin();
-		businessCategory();
+		try { 
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Oracle JDBC Driver not present or found");
+			e.printStackTrace();
+			return;
+		}
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:oracle:thin:@//158.130.111.68:1521/mydb.localhost", "system","Verna2813");
+		} catch (SQLException e) {
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return;
+		}
+		if (connection != null) {
+			Statement stmt = null;
+		    String createBusiness = "Create table Business("
+		    		+ "bid varchar(100),"
+		    		+ "name varchar(100),"
+		    		+ "full_address varchar(1000),"
+		    		+ "city varchar(25),"
+		    		+ "open varchar(10),"
+		    		+ "state varchar(100),"
+		    		+ "stars integer,"
+		    		+ "latitude float,"
+		    		+ "longitude float,"
+		    		+ "PRIMARY KEY(bid))";
+		    
+		    String createYelpUsers = "create table YelpUsers("
+		    		+ "user_id varchar(100),"
+		    		+ "name varchar(100),"
+		    		+ "avg_stars integer,"
+		    		+ "fans integer,"
+		    		+ "PRIMARY KEY(user_id))";
+		    
+		    String createCheckin = "create table CheckIn("
+		    		+ "bid varchar(100) PRIMARY KEY,"
+		    		+ "check_in_info integer,"
+		    		+ "FOREIGN KEY (bid) REFERENCES Business(bid) ON DELETE CASCADE)";
+		    
+		    String createBusinessCategory = "Create table Categories("
+		    		+ "bid varchar(100),"
+		    		+ "category varchar(100),"
+		    		+ "primary key(bid, category),"
+		    		+ "FOREIGN KEY (bid) REFERENCES Business(bid)  ON DELETE CASCADE)";
+		    
+		    String createUsers = "Create table AppUsers("
+		    		+ "user_id varchar(100),"
+		    		+ "password varchar(100),"
+		    		+ "email varchar(100),"
+		    		+ "is_facebook_login char(1),"
+		    		+ "PRIMARY KEY(user_id))";
+		    
+		    String createElite = "create table Elite("
+		    		+ "user_id varchar(100),"
+		    		+ "year integer,"
+		    		+ "primary key(user_id, year),"
+		    		+ "foreign key (user_id) references YelpUsers(user_id)  ON DELETE CASCADE)";
+		    
+		    String createNeighbourhood = "create table Neighborhood("
+		    		+ "bid varchar(100),"
+		    		+ "area varchar(100),"
+		    		+ "primary key(bid, area), "
+		    		+ "foreign key (bid) references Business(bid)  ON DELETE CASCADE)";
+		    
+		    String createHours = "create table BusinessHours ("
+		    		+ "bid varchar(100),"
+		    		+ "day varchar(20),"
+		    		+ "open_time varchar(10),"
+		    		+ "close_time varchar(10),"
+		    		+ "primary key(bid, day),"
+		    		+ "foreign key (bid) references Business(bid)  ON DELETE CASCADE)";	
+		    
+		    try {
+		        stmt = connection.createStatement();
+		        stmt.executeQuery(createBusiness);
+		        stmt.executeQuery(createYelpUsers);
+		        stmt.executeQuery(createUsers);
+		        stmt.executeQuery(createCheckin);
+		        stmt.executeQuery(createBusinessCategory);
+		        stmt.executeQuery(createElite);
+		        stmt.executeQuery(createNeighbourhood);
+		        stmt.executeQuery(createHours);
+		        
+		        business(stmt);
+				users(stmt);
+				checkin(stmt);
+				businessCategory(stmt);
+				//Elite(stmt);
+				//Neighbourhood(stmt);
+				//hours(stmt);
+				stmt.close();
+				connection.close();
+		    }catch(Exception e ){
+		    	e.printStackTrace();
+		    }
+		}   
+		
 	}
 	
-	public static void businessCategory(){
+	public static void Elite(Statement stmt){
+		try{
+			BufferedReader br = new BufferedReader(new FileReader("/Users/Adi/Documents/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_user.json"));
+			String line;
+			while ((line = br.readLine()) != null) {
+				JSONObject obj = new JSONObject(line);
+				String user_id = obj.getString("user_id");
+				JSONArray eliteArray = obj.getJSONArray("elite");
+				for(int i=0;i<eliteArray.length();i++){
+						String insert = "INSERT into Elite values(";
+						insert = insert + "\'" + user_id +"\'"+"," + eliteArray.get(i) + ")";
+						//System.out.println(insert);
+						stmt.executeQuery(insert);
+						//query to add (user_id,eliteArray.get(i)) to elite table;
+				}
+				
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void Neighbourhood(Statement stmt){
+		try {
+			 BufferedReader br = new BufferedReader(new FileReader("/Users/Adi/Documents/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json"));
+			 String line;
+			while ((line = br.readLine()) != null) {
+			    	JSONObject obj = new JSONObject(line);
+			    	String id = obj.getString("business_id");
+			    	JSONArray neighbourhood = obj.getJSONArray("neighborhoods");
+			    	for(int i=0;i<neighbourhood.length();i++){
+			    			// query to add (business_id, neighbourhood.get(i)) to neighbourhood table
+			    		String insert = "INSERT into Neighborhood values(";
+						insert = insert +"\'" + id +"\'"+"," + "\'" + neighbourhood.getString(i).replaceAll("'", "''")+"\'" + ")";
+						//System.out.println(insert);
+						stmt.executeQuery(insert);
+			    	}
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void hours(Statement stmt){
+		try {
+			 BufferedReader br = new BufferedReader(new FileReader("/Users/Adi/Documents/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json"));
+			 String line;
+			 ArrayList<String> days = new ArrayList<String>();
+			 days.add("Monday");days.add("Tuesday");days.add("Wednesday");days.add("Thursday");days.add("Friday");days.add("Saturday");days.add("Sunday");
+			 while ((line = br.readLine()) != null) {
+			    	JSONObject obj = new JSONObject(line);
+			    	String id = obj.getString("business_id");
+			    	JSONObject hoursObject = obj.getJSONObject("hours");
+			    	JSONObject j = null;
+			    	for(int i=0;i<days.size();i++){
+							try{
+								j = hoursObject.getJSONObject(days.get(i));
+								//sql query to insert into hours table (id, days.get(i),j.getString("open"),j.getString("close"))
+								String insert = "INSERT into BusinessHours values(";
+								insert = insert + "\'" + id +"\'," + "\'"+days.get(i) + "\'," + "\'" + j.getString("open")+ "\'," +"\'"+j.getString("close")+"\'"+ ")";
+								//System.out.println(insert);
+								stmt.executeQuery(insert);
+							}catch(Exception e){
+								//sql query to insert into hours table (id, days.get(i),0,0)
+								String insert = "INSERT into BusinessHours values(";
+								insert = insert + "\'" + id +"\'," + "\'" + days.get(i) + "\',\'0\',\'0\'" + ")";
+								//System.out.println(insert);
+								stmt.executeQuery(insert);
+							}
+						
+			    	}
+			 }
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public static void businessCategory(Statement stmt){
 		try{
 			BufferedReader br = new BufferedReader(new FileReader("/Users/Adi/Documents/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json"));
 			String line;
@@ -27,8 +202,12 @@ public class JSONParser {
 		    	JSONArray categoryArray = obj.getJSONArray("categories");
 		    	String category = "";
 		    	for(int i=0;i<categoryArray.length();i++){
-		    			category = categoryArray.getString(i);
+		    			category = categoryArray.getString(i).replaceAll("'", "''");
 		    			// WRITE SQL QUERY TO ADD (businessID,category) into table
+		    			String insert = "INSERT into Categories values(";
+						insert = insert + "\"" + businessID +"\'," + "\'" + category  + "\'"+ ")";
+						//System.out.println(insert);
+						stmt.executeQuery(insert);
 		    	}
 		    	
 			}
@@ -37,7 +216,7 @@ public class JSONParser {
 		}
 	}
 	
-	public static void checkin(){
+	public static void checkin(Statement stmt){
 		try{
 			BufferedReader br = new BufferedReader(new FileReader("/Users/Adi/Documents/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_checkin.json"));
 			String line;
@@ -49,99 +228,123 @@ public class JSONParser {
 				for(int i = 0; i<checkin.names().length(); i++){
 				    checkinCount += checkin.getInt(checkin.names().getString(i));
 				}
+				String insert = "INSERT into CheckIn values(";
+				insert = insert + "\'" + businessId +"\'," + checkinCount + ")";
+				//System.out.println(insert);
+				stmt.executeQuery(insert);
 			}
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
 	
-	public static void users(){
+	public static void users(Statement stmt){
 		try{
 			BufferedReader br = new BufferedReader(new FileReader("/Users/Adi/Documents/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_user.json"));
 			String line;
 			while ((line = br.readLine()) != null) {
 				JSONObject obj = new JSONObject(line);
 				String user_id = obj.getString("user_id");
-				String name = obj.getString("name");
+				String name = obj.getString("name").replaceAll("'", "'");
 				int avgStars = obj.getInt("average_stars");
-				JSONArray eliteArray = obj.getJSONArray("elite");
-		    	String elite = "";
-		    	for(int i=0;i<eliteArray.length();i++){
-		    		if(i == eliteArray.length()-1)
-		    			elite+=eliteArray.get(i);
-		    		else
-		    			elite+=eliteArray.get(i) + ",";
-		    	}
 		    	int fans = obj.getInt("fans");
+		    	String insert = "INSERT into YelpUsers values(";
+				insert = insert + "\'" + user_id +"\'," + "\'" + name + "\'," + avgStars + "," +fans + ")";
+				//System.out.println(insert);
+				stmt.executeQuery(insert);
+				
+				JSONArray eliteArray = obj.getJSONArray("elite");
+				for(int i=0;i<eliteArray.length();i++){
+						String insert1 = "INSERT into Elite values(";
+						insert1 = insert1 + "\'" + user_id +"\'"+"," + eliteArray.get(i) + ")";
+						//System.out.println(insert);
+						stmt.executeQuery(insert1);
+						//query to add (user_id,eliteArray.get(i)) to elite table;
+				}
+				
 			}
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
 	
-	public static void business() {
+	public static void business(Statement stmt) {
 		try {
-			BufferedReader br = new BufferedReader(new FileReader("/Users/Adi/Documents/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json"));
+			 BufferedReader br = new BufferedReader(new FileReader("/Users/Adi/Documents/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json"));
 			 String line;
 			 ArrayList<String> days = new ArrayList<String>();
 			 days.add("Monday");days.add("Tuesday");days.add("Wednesday");days.add("Thursday");days.add("Friday");days.add("Saturday");days.add("Sunday");
-			    while ((line = br.readLine()) != null) {
+			 while ((line = br.readLine()) != null) {
 			    	JSONObject obj = new JSONObject(line);
 			    	String id = obj.getString("business_id");
-			    	String name = obj.getString("name");
-			    	JSONArray neighbourhood = obj.getJSONArray("neighborhoods");
-			    	String n = "";
-			    	for(int i=0;i<neighbourhood.length();i++){
-			    		if(i == neighbourhood.length()-1)
-			    			n+=neighbourhood.get(i);
-			    		else
-			    			n+=neighbourhood.get(i) + ",";
-			    	}
-			    	String fullAddress = obj.getString("full_address");
-			    	String city = obj.getString("city");
+			    	String name = obj.getString("name").replaceAll("'", "''");
+			    	String fullAddress = obj.getString("full_address").replaceAll("'", "''");
+			    	fullAddress.replaceAll(",", ";");
+			    	String city = obj.getString("city").replaceAll("'", "''");
 			    	boolean openBoolean = obj.getBoolean("open");
 			    	String open = "";
 			    	if(openBoolean)
 			    		open = "true";
 			    	else
 			    		open = "false";
-			    	String state = obj.getString("state");
+			    	String state = obj.getString("state").replaceAll("'", "''");
 			    	int stars = obj.getInt("stars");
-			    	int latitude = obj.getInt("latitude");
-			    	int longitude = obj.getInt("longitude");
-			    	JSONObject hoursObject = obj.getJSONObject("hours");
-			    	String hours = "";
-			    	JSONObject j = null;
-			    	for(int i=0;i<days.size();i++){
-						if(i == days.size() - 1){
-							try{
-								j = hoursObject.getJSONObject(days.get(i));
-								hours += j.getString("open") + ",";
-								hours += j.getString("close");
-							}catch(Exception e){
-								hours += "0,";
-								hours += "0";
-							}
-						}
-						else{
-							try{
-								j = hoursObject.getJSONObject(days.get(i));
-								hours += j.getString("open") + ",";
-								hours += j.getString("close") + ",";
-							}catch(Exception e){
-								hours += "0,";
-								hours += "0,";
-							}
-						}
+			    	Double latitude = (Double)obj.get("latitude");
+			    	Double longitude = (Double)obj.get("longitude");
+			    	String insert = "INSERT into Business values(";
+					insert = insert + "\'" + id +"\'," + "\'" + name + "\'," + "\'" + fullAddress + "\'," +"\'"+city + "\'," + "\'" + open + "\'," + "\'" + state + "\'," +stars + "," + latitude + "," + longitude + ")";
+					//System.out.println(insert);
+					stmt.executeQuery(insert);
+					
+					JSONArray neighbourhood = obj.getJSONArray("neighborhoods");
+			    	for(int i=0;i<neighbourhood.length();i++){
+			    			// query to add (business_id, neighbourhood.get(i)) to neighbourhood table
+			    		String insert1 = "INSERT into Neighborhood values(";
+						insert1 = insert1 +"\'" + id +"\'"+"," + "\'" + neighbourhood.getString(i).replaceAll("'", "''")+"\'" + ")";
+						//System.out.println(insert);
+						stmt.executeQuery(insert1);
 			    	}
 			    	
-			    }
+			    	
+			    	
+			    	JSONObject hoursObject = obj.getJSONObject("hours");
+			    	JSONObject j = null;
+			    	for(int i=0;i<days.size();i++){
+							try{
+								j = hoursObject.getJSONObject(days.get(i));
+								//sql query to insert into hours table (id, days.get(i),j.getString("open"),j.getString("close"))
+								String insert2 = "INSERT into BusinessHours values(";
+								insert2 = insert2 + "\'" + id +"\'," + "\'"+days.get(i) + "\'," + "\'" + j.getString("open")+ "\'," +"\'"+j.getString("close")+"\'"+ ")";
+								//System.out.println(insert);
+								stmt.executeQuery(insert2);
+							}catch(Exception e){
+								//sql query to insert into hours table (id, days.get(i),0,0)
+								String insert2 = "INSERT into BusinessHours values(";
+								insert2 = insert2 + "\'" + id +"\'," + "\'" + days.get(i) + "\',\'0\',\'0\'" + ")";
+								//System.out.println(insert);
+								stmt.executeQuery(insert2);
+							}
+						
+			    	}
+			    	
+			    	
+			    	
+			    	JSONArray categoryArray = obj.getJSONArray("categories");
+			    	String category = "";
+			    	for(int i=0;i<categoryArray.length();i++){
+			    			category = categoryArray.getString(i).replaceAll("'", "''");
+			    			// WRITE SQL QUERY TO ADD (businessID,category) into table
+			    			String insert3 = "INSERT into Categories values(";
+							insert3 = insert3 + "\"" + id +"\'," + "\'" + category  + "\'"+ ")";
+							//System.out.println(insert);
+							stmt.executeQuery(insert3);
+			    	}
+			    	
+			 }   			
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 		   
-		
-
 	}
 
 }
