652bd26d7729440c404e751a6ab2c7cc6daa85c6
ssn2013 <ssn2009@gmail.com>
2015-04-11 16:29:06 -0400

    dying

--- HeatMapServer/src/com/datformers/utils/DatabaseUtil.java
+++ HeatMapServer/src/com/datformers/utils/DatabaseUtil.java
@@ -7,7 +7,8 @@ public class DatabaseUtil {
 	public static String PASSWORD = "Verna2813";
 	public static String getURL(String ip) {
 		IP = ip;
-		URL = "jdbc:oracle:thin:@//"+IP+":1521/mydb.localhost";
+		//URL = "jdbc:oracle:thin:@//"+IP+":1521/mydb.localhost";
+		URL = "jdbc:oracle:thin:@//158.130.106.114:1521/mydb.localhost";
 		return URL;
 	}
 }
