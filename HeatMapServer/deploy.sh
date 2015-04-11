e423e91c5c85109d57db9c16809188a9f1d3e9d2
ssn2013 <ssn2009@gmail.com>
2015-04-11 15:37:28 -0400

    removing deploy.sh

--- HeatMapServer/deploy.sh
+++
@@ -1,4 +0,0 @@
-rm -rf /home/sruthi/Downloads/apache-tomcat-6.0.43/webapps/heatmapserver /home/sruthi/Downloads/apache-tomcat-6.0.43/webapps/heatmapserver.war
-echo "Removed from tomcat webapps" 
-ant servlet-war
-mv heatmapserver.war /home/sruthi/Downloads/apache-tomcat-6.0.43/webapps
