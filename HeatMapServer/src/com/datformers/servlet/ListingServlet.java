3641cb4919eff023746aff1d0f551f50ea771063
Adarsh Vakkaleri Sateesh <Adi@Adarshs-MacBook-Pro.local>
2015-04-12 23:19:54 -0400

    Added a random summary

--- HeatMapServer/src/com/datformers/servlet/ListingServlet.java
+++ HeatMapServer/src/com/datformers/servlet/ListingServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,8 +14,11 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import com.datformers.ReviewSummary.ReviewSummary;
+import com.datformers.database.MongoDBWrapper;
 import com.datformers.database.OracleDBWrapper;
 import com.datformers.utils.DatabaseUtil;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 
 public class ListingServlet extends HttpServlet{
 	private OracleDBWrapper dbWrapper = new OracleDBWrapper(DatabaseUtil.getURL("158.130.106.114"), DatabaseUtil.UERNAME,DatabaseUtil.PASSWORD);	
@@ -25,12 +29,14 @@ public class ListingServlet extends HttpServlet{
 		String city="";
 		String category="";
 		String bid = "";
-		String summary = "";
+		String review = "";
 		String businessName="";
 		String address="";
 		String stars = "";
+		String open="";
 		int startIndex=1;
 		int endIndex = 0;
+		ArrayList<DBObject> res = new ArrayList<DBObject>();
 		city = request.getParameter("city");
 		category= request.getParameter("category");
 		startIndex=Integer.parseInt(request.getParameter("index"));
@@ -42,18 +48,43 @@ public class ListingServlet extends HttpServlet{
 		ResultSet set = dbWrapper.executeQuery(queryString);
 		JSONArray ja = new JSONArray();
 		
+		MongoDBWrapper mdb = new MongoDBWrapper("158.130.106.114", 27017,
+				"Reviews");
+		mdb.createConnection();
+		
+
+		
+
+		
+
+		
 		try {
 			while(set.next()) {
 				bid=set.getString("bid");
 				businessName = set.getString("name");
 				stars = set.getString("stars");
 				address=set.getString("full_address");
+				open=set.getString("open");
+				BasicDBObject Query = new BasicDBObject();
+				Query.put("business_id", bid);
+				BasicDBObject fields = new BasicDBObject();
+				fields.put("text", 1);
+				res = mdb.executeQuery(Query, fields);
+				for (int i = 0; i < res.size(); i++) {
+					DBObject current = res.get(i);
+					if(String.valueOf(current.get("text")).length() < 70){
+						review = String.valueOf(current.get("text"));
+						break;
+					}
+				}
 				
 				JSONObject jo = new JSONObject();
 				jo.put("bid", bid);
 				jo.put("name", businessName);
 				jo.put("address", address);
 				jo.put("stars", stars);
+				jo.put("open", open);
+				jo.put("review", review);
 				ja.add(jo);
 			}
 			
